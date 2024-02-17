950201a3b5412c695eb6b86e0ea6361872fec9c9
João Xavier <fastjonh@gmail.com>
2024-02-17 04:41:53 -0300

    acho tipos longos bonitos as vezes...

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
-use axum::Router;
-use models::TransientError;
-
 mod db;
+mod handlers;
 mod models;
 mod routes;
 
 #[tokio::main]
-async fn main() -> Result<(), TransientError> {
+async fn main() -> Result<(), models::TransientError> {
     db::grant_database_tables()?;
     db::seed_data()?;
 
-    let app = Router::new().nest("/clientes/:id", routes::router());
+    let app = axum::Router::new().nest("/clientes/:id", routes::router());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:9999").await?;
 
