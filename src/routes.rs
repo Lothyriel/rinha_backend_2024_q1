31c76bd333e1d338d2da97f33af6dac8aff206fb
João Xavier <fastjonh@gmail.com>
2024-02-16 22:53:48 -0300

    .

--- src/routes.rs
+++ src/routes.rs
@@ -1,10 +1,35 @@
 use axum::{
     extract::{Json, Path},
+    http::StatusCode,
+    response::IntoResponse,
     routing::{get, post},
     Router,
 };
+use rusqlite::OptionalExtension;
 
 use crate::{db::get_connection, models::*};
+
+#[derive(thiserror::Error, Debug)]
+pub enum ErrorResponse {
+    #[error("{0}")]
+    Transient(#[from] TransientError),
+    #[error("Client with id {{0}} not found")]
+    ClientNotFound(ClientId),
+    #[error("Not enough balance to complete this transaction")]
+    NoBalance,
+}
+
+impl IntoResponse for ErrorResponse {
+    fn into_response(self) -> axum::response::Response {
+        let status_code = match &self {
+            ErrorResponse::ClientNotFound(_) => StatusCode::NOT_FOUND,
+            ErrorResponse::NoBalance => StatusCode::UNPROCESSABLE_ENTITY,
+            ErrorResponse::Transient(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        };
+
+        (status_code, self).into_response()
+    }
+}
 
 pub fn router() -> Router {
     Router::new()
@@ -16,9 +41,15 @@ async fn add_transaction(
     Path(client_id): Path<ClientId>,
     Json(data): Json<TransactionRequest>,
 ) -> Result<Json<TransactionResponse>, ErrorResponse> {
+    let data = get_client(client_id)?;
+
+    todo!()
+}
+
+fn get_client(client_id: u32) -> Result<ClientData, ErrorResponse> {
     let conn = get_connection()?;
 
-    let data = conn
+    let client = conn
         .query_row(
             "SELECT
                balance,
@@ -27,15 +58,17 @@ async fn add_transaction(
              WHERE id = :client_id;",
             [client_id],
             |row| {
-                Ok(TransactionResponse {
+                Ok(ClientData {
+                    id: client_id,
                     balance: row.get(0)?,
                     limit: row.get(1)?,
                 })
             },
         )
-        .optional()?;
+        .optional()?
+        .ok_or_else(|| ErrorResponse::ClientNotFound(client_id))?;
 
-    Ok(Json(data))
+    Ok(client)
 }
 
 async fn get_extract(
